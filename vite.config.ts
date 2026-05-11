a8b9c637b1eaa288afe7c05b77ab563b40a931fc
Trần Thanh Phúc <tranthanhphucbvdkgr@gmail.com>
2026-05-11 15:42:47 +0700

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -6,6 +6,7 @@ import tailwindcss from '@tailwindcss/vite';
 export default defineConfig(({ mode }) => {
     const env = loadEnv(mode, '.', '');
     return {
+        Base: '/Restorephoto/',
       server: {
         port: 3000,
         host: '0.0.0.0',
